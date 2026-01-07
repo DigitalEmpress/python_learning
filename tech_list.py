af61138a1e732884d12d1a6ffbabdebbd61a16c6
DigitalEmpress <88089020+DigitalEmpress@users.noreply.github.com>
2026-01-07 11:58:29 -0600

    Add files via upload
    add list_tech.py file to python_learning repo

---
+++ tech_list.py
@@ -0,0 +1,18 @@
+faang = ['Facebook', 'Apple', 'Amazon', 'Netflix', 'Google']
+big_tech = ['Microsoft', 'IBM', 'Oracle', 'Intel', 'Cisco']
+all_tech_companies = faang + big_tech
+
+message = f"My dream big tech and faang company to work for are {faang[-2]} and {big_tech[0]}."
+
+applied_companies = []
+applied_companies.append('GM')
+applied_companies.append('Oracle')
+applied_companies.append('IBM')
+applied_companies.append('Experian')
+applied_companies.append('Visa')
+
+applied_message = f"\n I have applied to {len(applied_companies)} companies so far. They are {applied_companies}."
+
+print(message)
+print(applied_message)
+
